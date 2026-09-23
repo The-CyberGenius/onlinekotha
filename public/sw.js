511a2854f655a3e9a49d01af03e6dd7273a78a0d
The-CyberGenius <sshivaprajapat@gmail.com>
2026-09-23 23:12:50 +0530

    Bump service worker cache to v27 to clear mobile cache for filter modal and wrapped updates

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 // Service Worker for Kotha PWA
-const CACHE_NAME = 'kotha-v26';
-const DYNAMIC_CACHE = 'kotha-dynamic-v26';
+const CACHE_NAME = 'kotha-v27';
+const DYNAMIC_CACHE = 'kotha-dynamic-v27';
 const STATIC_ASSETS = [
     '/css/style.css',
     '/js/tailwind.js',
